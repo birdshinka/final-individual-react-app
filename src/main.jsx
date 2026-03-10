ae72404447087aa44566db4d9bb0eb53b49c0151
Shogo Muranaka <121185536+birdshinka@users.noreply.github.com>
2026-03-10 12:34:29 -0700

    Created Navbar Files

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './Nav.jsx'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Nav />
     <App />
   </StrictMode>,
 )
