8b672e29c5990112ada3b2021c7ccc8f95aa6b60
Shogo Muranaka <121185536+birdshinka@users.noreply.github.com>
2026-03-10 20:33:18 +0000

    Edited Files and Removed Margin around the App

---
+++ src/Footer.jsx
@@ -0,0 +1,12 @@
+import './Footer.css'
+
+function Footer(){
+    return(
+        <div>
+            <footer>
+                <p>&copy;2026 CompSci</p>
+            </footer>
+        </div>
+    )
+}
+export default Footer
