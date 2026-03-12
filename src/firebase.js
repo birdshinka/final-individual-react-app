613718a11ab561b5846666e71226c5fcbbd13e01
Shogo Muranaka <121185536+birdshinka@users.noreply.github.com>
2026-03-12 12:54:08 -0700

    Added google.jsx

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,19 @@
-const firebaseConfig = {  // FROM YOUR CONFIG IN FIREBASE
-    apiKey: 'YOUR_API_KEY',
-    authDomain: 'YOUR_PROJECT_ID.firebaseapp.com',
-    projectId: 'YOUR_PROJECT_ID',
-    storageBucket: 'YOUR_PROJECT_ID.appspot.com',
-    messagingSenderId: 'YOUR_MSG_SENDER_ID',
-    appId: 'YOUR_APP_ID'
-  };
-  
-  const app = initializeApp(firebaseConfig);
-  
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAjfvzfLJD7-VkeQCWl7pF9Nt2vg8blMJg",
+  authDomain: "react-firebase-app-274f9.firebaseapp.com",
+  projectId: "react-firebase-app-274f9",
+  storageBucket: "react-firebase-app-274f9.firebasestorage.app",
+  messagingSenderId: "703958973623",
+  appId: "1:703958973623:web:93f4587b0c017653d2e132"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
